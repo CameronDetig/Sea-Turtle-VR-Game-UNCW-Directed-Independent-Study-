e84f5f1dcc5346310dac8a24c2caaa5bbce75f23
Cameron Detig <camerondetig@gmail.com>
2019-02-11 19:21:55 -0500

    First Commit
    First Commit.

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    private Color waterColor;
+    private float waterDensity;
+
+    void Start()
+    {
+        waterColor = new Color(0.22f, 0.65f, 0.77f, 0.5f);
+        waterDensity = 0.1f;
+    }
+
+    void Update()
+    {
+        RenderSettings.fogColor = waterColor;
+        RenderSettings.fogDensity = waterDensity;
+    }
+}
