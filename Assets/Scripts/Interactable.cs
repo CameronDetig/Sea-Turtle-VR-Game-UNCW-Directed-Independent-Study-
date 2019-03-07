efc0520e51fc9278508124a92e5a6201cef3b1bd
CameronDetig <camerondetig@gmail.com>
2019-03-07 16:34:54 -0500

    Pickup

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Interactable : MonoBehaviour
+{
+    [HideInInspector]
+    public Hand m_ActiveHand = null;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Recycling" || col.gameObject.name == "Turtle")
+        {
+            print("Collision");
+            Destroy(this.gameObject);
+        }
+    }
+}
