9d3bb9b8dd40b795a8e799914912f3eaddd20794
CameronDetig <camerondetig@gmail.com>
2019-03-19 21:57:52 -0400

    Objects Move Towards Turtle

--- Assets/Scripts/JellyFish.cs
+++ Assets/Scripts/JellyFish.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class JellyFish : MonoBehaviour
 {
+    public float speed = .5f;
+    public GameObject turtle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        turtle = GameObject.FindWithTag("turtle");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float step = speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, turtle.transform.position, step);
     }
 
-    void FixedUpdate()
-    {
-        GetComponent<Rigidbody>().AddForce(0,1,0);
-    }
+    //void FixedUpdate()
+    //{
+    //    GetComponent<Rigidbody>().AddForce(0,1,0);
+    //}
 }
